672974da2034639900edbbc47acfd4644b9a9793
Jonathon Sturdevant <jonathon.sturdevant@gmail.com>
2013-10-23 13:35:48 -0400

    Rearranged the navigation menu to be list based to assist with responsive web design.

--- golf-league/navigation.inc.php
+++ golf-league/navigation.inc.php
@@ -4,41 +4,28 @@ $parts = Explode('/', $currentFile);
 $thisPage = $parts[count($parts) - 1];
 ?>
 
-    <div class="navigation">
-        <?php
-        if ($thisPage != "index.php") { 
-        ?>
-        <a href="/index.php">
-            <span class="nav-key">Home</span>
-        </a>
-        <?php
-        }
-        if ($thisPage != "temp-schedule.php") {
-        ?>
-        <a href="/temp-schedule.php">
-            <span class="nav-key">Schedule</span>
-        </a>
-        <?php 
-        }
-        if ($thisPage != "rules.php") {
-        ?>
-        <a href="/rules.html">
-            <span class="nav-key">Rules</span>
-        </a>
-        <?php
-        } if ($thisPage != "subs.php") { 
-        ?>
-        <a href="/member/subs.php">
-            <span class="nav-key">Subs</span>
-        </a>
-        <?php 
-        }
-        if ($_SESSION["admin"] == 1) {
-       	?>
-            <a href="/admin/index.php">
-                <span class="admin-nav-key">Admin Functions</span>
-            </a>
-        <?php
-        } 
-        ?>
-    </div>
+    <nav role="navigation">
+        <ul>
+            <li>
+                <a href="/index.php">Home</a>
+            </li>
+            <li>
+                <a href="/temp-schedule.php">Schedule</a>
+            </li>
+            <li>
+                <a href="/rules.html">Rules</a>
+            </li>
+            <li>
+                <a href="/member/subs.php">Subs</a>
+            </li>
+            <?php
+                if ($_SESSION["admin"] == 1) {
+            ?>
+                    <li>
+                        <a href="/admin/index.php">Admin Functions</a>
+                    </li>
+            <?php 
+                }
+            ?>
+        </ul>
+    </nav>
