c640aa049ab2dbd83a7cbd5af7c349f431be6568
Jonathon Sturdevant <jonathon.sturdevant@gmail.com>
2016-03-23 21:07:11 -0400

    removed references to an old phpmyadmin and email page

--- golf-league/src/php/config.inc.php
+++ golf-league/src/php/config.inc.php
@@ -9,11 +9,6 @@ $db_server = "107.180.51.10";
 $db_user = "bcbsscgl";
 $db_password = "hnTCZXEN2Ab6tb";
 $db_database = "bcbsscgl";
-/*
- * phpMyAdmin - https://p3nlmysqladm001.secureserver.net/grid50/3447/index.php?uniqueDnsEntry=bcbsscgl.db.9045345.hostedresource.com
- * 
- * email - https://login.secureserver.net/index.php?app=wbe
- */
 
 // setup the database
 $link = mysql_connect($db_server, $db_user, $db_password) or die ("Could not connect to the database");
