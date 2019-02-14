eee183a740d5b5be232bcc3bd36b682b3a593a23
Jonathon Sturdevant <jonathon.sturdevant@gmail.com>
2019-02-13 22:19:01 -0500

    added the capability to remove posts if logged in as an admin

--- golf-league/src/php/service/BlogService.php
+++ golf-league/src/php/service/BlogService.php
@@ -11,6 +11,17 @@ if (isset($_GET["operation"])) {
 	    	$blog = $dao->getBlogInformation();
 	    	echo json_encode($blog);
 	    	break;
+	    case "delete":
+	        // get the postId from the request
+	        if (array_key_exists("postId", $_GET)) {
+	        	$postId = $_GET["postId"];
+	        	$dao = new BlogDAO();
+	        	$dao->deleteBlogPost($postId);
+	        	echo json_encode("Deleted the post");
+	        } else {
+	        	echo json_encode("Provide the id of the post to delete");
+	        }
+	        break;
 	    default:
 	    	echo "Unsupported operation - " . $operation;
 	    	break;
