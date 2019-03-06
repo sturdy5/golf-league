43b7dae2a4946b3f14cefef91bcdc6d799040b00
Jonathon Sturdevant <jonathon.sturdevant@gmail.com>
2019-03-05 23:21:31 -0500

    fixed spacing

--- golf-league/src/php/service/BlogService.php
+++ golf-league/src/php/service/BlogService.php
@@ -2,29 +2,29 @@
 include_once 'requires.inc.php';
 
 if (isset($_GET["operation"])) {
-	// the operation will map to one of the following values:
-	//   - get 
-	$operation = $_GET["operation"];
-	switch ($operation){
-	    case "get":
-	    	$dao = new BlogDAO();
-	    	$blog = $dao->getBlogInformation();
-	    	echo json_encode($blog);
-	    	break;
-	    case "delete":
-	        // get the postId from the request
-	        if (array_key_exists("postId", $_GET)) {
-	        	$postId = $_GET["postId"];
-	        	$dao = new BlogDAO();
-	        	$dao->deleteBlogPost($postId);
-	        	echo json_encode("Deleted the post");
-	        } else {
-	        	echo json_encode("Provide the id of the post to delete");
-	        }
-	        break;
-	    default:
-	    	echo "Unsupported operation - " . $operation;
-	    	break;
-	}
+    // the operation will map to one of the following values:
+    //   - get 
+    $operation = $_GET["operation"];
+    switch ($operation){
+        case "get":
+            $dao = new BlogDAO();
+            $blog = $dao->getBlogInformation();
+            echo json_encode($blog);
+            break;
+        case "delete":
+            // get the postId from the request
+            if (array_key_exists("postId", $_GET)) {
+                $postId = $_GET["postId"];
+                $dao = new BlogDAO();
+                $dao->deleteBlogPost($postId);
+                echo json_encode("Deleted the post");
+            } else {
+                echo json_encode("Provide the id of the post to delete");
+            }
+            break;
+        default:
+            echo "Unsupported operation - " . $operation;
+            break;
+    }
 }
 ?>
